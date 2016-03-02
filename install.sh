43395ec7a351b36194a858d6bb99458558b47f29
hriesco <hectorriesco@hotmail.com>
2016-03-02 20:18:32 +0100

    Installation script for GNU/Linux OS

--- install.sh
+++ install.sh
@@ -2,27 +2,32 @@
 
 if [ $UID -eq 0 ]; then
 	echo "Installing Command cheatsheet in your O.S."
+
 	cp -r ../ccsheet /opt/
-	echo '"# Command Cheatsheet Alias -------------------"' >> ~/.bashrc
-	echo 'alias ccs="python /opt/ccsheet/ccs.py"' >> ~/.bashrc
-	echo 'alias ccsl="python /opt/ccsheet/ccs.py -l"' >> ~/.bashrc
-	echo 'alias ccsgui="python /opt/ccsheet/ccsgui.py"' >> ~/.bashrc
-	echo '"-----------------------------------------------"' >> ~/.bashrc
+	groupadd ccsheet
+	chgrp -R ccsheet /opt/ccsheet
+	chmod -R 770 /opt/ccsheet
+
+	echo '# Command Cheatsheet Alias -------------------' >> /root/.bashrc
+	echo 'alias ccs="python /opt/ccsheet/ccs.py"' >> /root/.bashrc
+	echo 'alias ccsl="python /opt/ccsheet/ccs.py -l"' >> /root/.bashrc
+	echo 'alias ccsgui="python /opt/ccsheet/ccsgui.py"' >> /root/.bashrc
+	echo '-----------------------------------------------' >> /root/.bashrc
 	
 	for user in `ls /home/ | grep -v "lost+found"`
 	do
 		next=false
 		while ! $next 
 		do
-			echo $next
 			read -p "Install for user: $user [y/n] " opc 
 			if [ $opc = 'y' ]; then
 				next=true
-				echo '"# Command Cheatsheet Alias -------------------"' >> /home/$(user)/.bashrc
-				echo 'alias ccs="python /opt/ccsheet/ccs.py"' >> /home/$(user)/.bashrc
-				echo 'alias ccsl="python /opt/ccsheet/ccs.py -l"' >> /home/$(user)/.bashrc
-				echo 'alias ccsgui="python /opt/ccsheet/ccsgui.py"' >> /home/$(user)/.bashrc
-				echo '"-----------------------------------------------"' >> /home/$(user)/.bashrc
+				usermod -a -G ccsheet $user
+				echo '# Command Cheatsheet Alias -------------------' >> /home/$user/.bashrc
+				echo 'alias ccs="python /opt/ccsheet/ccs.py"' >> /home/$user/.bashrc
+				echo 'alias ccsl="python /opt/ccsheet/ccs.py -l"' >> /home/$user/.bashrc
+				echo 'alias ccsgui="python /opt/ccsheet/ccsgui.py"' >> /home/$user/.bashrc
+				echo '#-----------------------------------------------' >> /home/$user/.bashrc
 			elif [ $opc = 'n' ]; then
 				next=true
 			else
@@ -30,8 +35,9 @@ if [ $UID -eq 0 ]; then
 			fi
 		done
 	done
-	echo "Installation done"
-	echo "Now you con put in your terminal the next commands to access the application"
+	clear
+	echo "Installation done..."
+	echo "When you restart terminal you can put the next commands to access the application"
 	echo "ccs    - command cheatsheet application"
 	echo "ccsl   - command cheatsheet list categories"
 	echo "ccsgui - command cheatsheet gui for make new cheatsheet"
